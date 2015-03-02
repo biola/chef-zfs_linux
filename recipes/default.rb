7250ff914ac0bb70658f3de34c7fe8914040f169
troyready <troy@troyready.com>
2015-03-02 12:59:05 -0800

    update copyright to 2015

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,20 @@
 # Cookbook Name:: zfs_linux
 # Recipe:: default
 #
-# Copyright 2013, Biola University
-# Copyright (c) 2012, Cameron Johnston
-#
+# Copyright 2015 Biola University
+# Copyright 2012 Cameron Johnston
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 case node['platform']
 when "ubuntu"
@@ -50,7 +49,9 @@ when "ubuntu"
       action :nothing
     end
 
-    group node['zol']['dev_group']
+    group node['zol']['dev_group'] do
+      system true
+    end
 
     template '/etc/udev/rules.d/91-zfs-permissions.rules' do
       source '91-zfs-permissions.rules.erb'
